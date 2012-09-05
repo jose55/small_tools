e5f134e15e7be97e820a4b932432267cc637416f
Jango <j.4@gmx.at>
2012-09-05 17:05:47 +0200

    learned again and made another test to keep track of what i should know.

---
+++ TestMeMore.cpp
@@ -0,0 +1,77 @@
+/* A little C++ Programm to test more things */
+
+// Include standard library
+#include <iostream>
+#include <tuple>
+#include <functional> //for ref function
+#include <deque> //for double ended queue
+
+
+using namespace std;
+
+int foo (int a)
+{
+    a++;
+    return a;
+}
+
+int foo1 (int & a)
+{
+    a++;
+    return a;
+}
+
+// Main programm code starts here
+int main ()
+{
+    int a = 1;
+	cout << "Print a: " << a << endl;
+    cout << "Print Pointer to a: " << &a << endl;
+    // passing the variable only changes it in the function, but not in main
+    cout << "Print foo(a): " << foo(a) << endl;
+    cout << "Print a: " << a << endl;
+    // this is passing the variable by reverence: therefore it can be changed
+    cout << "Print foo1(&a): " << foo1(a) << endl;
+    cout << "Print a: " << a << endl;
+
+	//automatic made tuple
+    auto t = make_tuple(43,45,"Hello World!");
+	//print hello world with get value at pos 2
+    cout << get<2>(t) << endl;
+
+    int c, d;
+    //assign c with second tuple value
+    tie(ignore,c,ignore) = t;
+    // same with d
+    d = get<1>(t);
+    cout << "this is c: " << c << endl;
+    cout << "this is d: " << d << endl;
+
+    //tuple types can be reverences
+    string s ("hi");
+    int i (42);
+    tuple<string&,int&> tup(s,i);
+    cout << "tup is: " << get<0>(tup) << ", " << get<1>(tup) << endl;
+    s = "hi changed me";
+    i = 12;
+    cout << "tup is changed: " << get<0>(tup) << ", " << get<1>(tup) << endl;
+
+    //rest reference function ref
+    auto tup1 = make_tuple(ref(s),4);
+    s = s.append(" again"); //append to string
+    cout << "tup1 is: " << get<0>(tup1) << ", " << get<1>(tup1) << endl;
+
+
+    //deque test
+    deque<int> de;
+    // fill this thing from the front
+    int x{};
+    for(x; x<10;++x) {
+		de.push_front(x);
+    }
+    // read it to cout
+    for(auto elem : de) {
+		cout << elem << " ";
+    }
+    cout << endl;
+}
