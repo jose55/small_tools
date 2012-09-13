3ddae72e58ba611ae67359c20f3eb82ca1fa7aa2
Jango <j.4@gmx.at>
2012-09-13 18:54:48 +0200

    added a lambda expression, so i wont forget it

--- TestPredicates.cpp
+++ TestPredicates.cpp
@@ -39,30 +39,48 @@ Movie addMovie (string title, long year)
 	return my_movie;
 }
 
+// overload output operator << to be able to print the deque with Movies
+std::ostream& operator<< (std::ostream& os, deque<Movie>& dM) {
+    for (auto elem : dM) {
+        elem.print();
+    }
+    return os;
+}
+
 // main program starts here
 int main ()
 {
 	// a deque is used to store the list of Movies
 	deque<Movie> movies;
 	// fill the list
-	movies.push_back(addMovie("Friede den Huetten",2007));
 	movies.push_back(addMovie("Matrix",1976));
 	movies.push_back(addMovie("Avatar",2007));
 	movies.push_back(addMovie("Lion King",1994));
 	movies.push_back(addMovie("Star Wars",1976));
 
 	// print without sorting
-	cout << "Without sorting: " << endl;
-	for (auto elem : movies) {
-		elem.print();
-	}
+	cout << "Without sorting: " << endl << movies;
 
 	// sort this list using Predicates as cirterion
 	sort(movies.begin(), movies.end(), movieSorting);
 
 	// print the sorted list
-	cout << "Sorting done: " << endl;
-	for (auto elem : movies) {
-		elem.print();
-	}
+	cout << "Sorting done: " << endl << movies;
+
+	// add more movies
+	movies.push_back(addMovie("Xaver",1956));
+	movies.push_back(addMovie("Friede den Huetten",1956));
+
+	cout << "Added more movies: " << endl << movies;
+
+	// the same could have been done with a lambda expression; advantage: code at one place only!
+	sort(movies.begin(), movies.end(), [] (const Movie m1, const Movie m2) {
+											return m1.Year < m2.Year ||
+												(m1.Year == m2.Year && m1.Title < m2.Title);
+										});
+
+	// print a last time
+	cout << "Sorted using a Lambda expression: " << endl << movies << endl;
+
+	return 0;
 }
